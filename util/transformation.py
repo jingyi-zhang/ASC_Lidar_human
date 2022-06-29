efa44ae17d1cb65f1f209223a55463aa9483e4f1
Maoqihong <1275465626@qq.com>
2022-06-29 17:02:07 +0800

    数据处理pipeline, server端

--- util/transformation.py
+++ util/transformation.py
@@ -1,3 +1,5 @@
+import math
+
 from matplotlib.pyplot import axis
 
 import numpy as np
@@ -44,6 +46,95 @@ def icp_without_rotation(source_points, target_points, target_kdtree, iteration=
         source_points_copy += offset
     return translation
 
+# 使用模拟退火算法；只进行x,y轴配准；最大化点云中位于smpl mesh表面点的数量；
+def mqh_sa_icp_wo_r(smpl_v, pc, init_trans, trans_move_limit=0.2, dist_threshold=0.01, T=1, Tmin=0.5, k=5, z=0):
+    pc = pc.astype(np.float32)
+    kdtree = pcl.PointCloud(smpl_v.astype(np.float32)).make_kdtree_flann()
+
+    def objective_func(t):
+        ind, dist = kdtree.nearest_k_search_for_cloud(pcl.PointCloud(pc + t), 1)
+        mask = dist < dist_threshold
+        return mask.sum() / 10, mask
+
+    #init_trans = smpl_v.mean(axis=0) - pc.mean(axis=0)
+    trans = init_trans.astype(np.float32).copy()
+    trans[2] = z
+    o, m = objective_func(trans)
+    #print(f'mqh_sa_icp_wo_r, init_o:{o}, trans_limit:{trans_move_limit}, T:{T}, Tmin:{Tmin}, K:{k}')
+    while T >= Tmin:
+        for i in range(k):
+            #new_trans = trans + (np.random.rand(3) - 0.5) * np.array([1, 1, 0.5]) * T
+            new_trans = trans + (np.random.rand(3) - 0.5) * 2 * trans_move_limit * T
+            new_trans[2] = z
+            new_trans = new_trans.astype(np.float32)
+
+            if np.linalg.norm(init_trans-new_trans) < trans_move_limit:    #此处本来是用于判定生成的new_trans是否合理的
+                new_o, new_m = objective_func(new_trans)
+                if new_o - o > 0:
+                    #print(f'{o:.3f}->{new_o:.3f} accept')
+                    trans, o, m = new_trans, new_o, new_m
+                else:
+                    p = math.exp((new_o - o) / T)
+                    r = np.random.uniform(low=0, high=1)
+                    if r < p:
+                        #print(f'{o:.3f}->{new_o:.3f} accept by p:{p:.3f}')
+                        trans, o, m = new_trans, new_o, new_m
+                    else:
+                        #print(f'{o:.3f}->{new_o:.3f} reject by p:{p:.3f}')
+                        pass
+                #print(T, i, trans, o)
+
+        T *= 0.6  # 降温函数，也可使用T=0.9T
+        #print(f'fitness:{o:.3f}, T:{T:.3f}')
+    return trans, m.flatten()
+
+# 只进行x,y轴配准；最大化点云中位于smpl mesh表面点的数量；
+def mqh_icp_wo_r(smpl_v, pc, init_trans, trans_move_limit=0.2, dist_threshold=0.01, T=1, Tmin=0.1, k=5, z=0):
+    pc = pc.astype(np.float32)
+    smpl_v = smpl_v.astype(np.float32)
+    kdtree = pcl.PointCloud(smpl_v).make_kdtree_flann()
+    pc_center = np.average(pc, axis=0)
+
+    #TODO: pcl.PointCloud 应用trans
+    def objective_func(t):
+        ind, dist = kdtree.nearest_k_search_for_cloud(pcl.PointCloud(pc + t), 1)
+        return (dist < dist_threshold).sum()
+
+
+    #init_trans = smpl_v.mean(axis=0) - pc.mean(axis=0)
+    trans = init_trans.copy()
+    trans[2] = z
+
+    o = objective_func(trans)
+
+    while T >= Tmin:
+        for i in range(k):
+            #new_trans = trans + (np.random.rand(3) - 0.5) * np.array([1, 1, 0.5]) * T
+            noice_trans = trans + (np.random.rand(3) - 0.5) * 2 * trans_move_limit * T
+            noice_trans[2] = z
+            noice_trans = noice_trans.astype(np.float32)
+
+            ind, dist = kdtree.nearest_k_search_for_cloud(pcl.PointCloud(pc + noice_trans), 1)
+            #o = (dist < dist_threshold).sum()
+
+            new_trans = np.average(smpl_v[ind.flatten()], axis=0) - pc_center
+            new_trans[2] = z
+
+            if np.linalg.norm(init_trans-new_trans) < trans_move_limit:    #此处本来是用于判定生成的new_trans是否合理的
+                new_o = objective_func(new_trans)
+                if new_o - o > 0:
+                    trans, o = new_trans, new_o
+                else:
+                    p = math.exp((new_o - o) / T)
+                    r = np.random.uniform(low=0, high=1)
+                    if r < p:
+                        trans, o = new_trans, new_o
+
+                print(T, i, trans, o)
+
+        T *= 0.6  # 降温函数，也可使用T=0.9T
+    return trans
+
 
 def get_mocap_to_lidar_translation(mocap_points, lidar_points, lidar_to_mocap_rotation):
     source_points = mocap_points.astype(np.float32)
@@ -51,6 +142,13 @@ def get_mocap_to_lidar_translation(mocap_points, lidar_points, lidar_to_mocap_ro
         lidar_points, lidar_to_mocap_rotation).astype(np.float32)
     target_kdtree = pcl.PointCloud(target_points).make_kdtree_flann()
     return icp_without_rotation(source_points, target_points, target_kdtree)
+
+def mqh_get_mocap_to_lidar_translation(mocap_points, lidar_points, lidar_to_mocap_rotation):
+    source_points = mocap_points.astype(np.float32)
+    target_points = affine(
+        lidar_points, lidar_to_mocap_rotation).astype(np.float32)
+    source_kdtree = pcl.PointCloud(source_points).make_kdtree_flann()
+    return -icp_without_rotation(target_points, source_points, source_kdtree)
 
 
 def get_mocap_to_lidar_rotation(mocap_points, lidar_points, lidar_to_mocap_rotation):
